7384a46ecd142d03f9eb0007d18bcd14e33b5221
Tobias Lundin <tobias.lundin@gmail.com>
2020-03-04 05:50:06 +0100

    render header overlay as early as we can

--- tolu-extension.js
+++ tolu-extension.js
@@ -4,22 +4,15 @@ chrome.storage.sync.get(({resCacheHeaders}) => {
     const res = [
       { name: 'x-nrk-outputcache-hit', label: 'output cache'},
       { name: 'x-cache', label: 'akamai' }
-    ].map(({name, label}) => ({
-      style: getStyle(!!resCacheHeaders.find(h => h.name === name)),
-      label,
-    }));
-    document.body.insertAdjacentHTML('beforeend', /*html*/`
-      <div style="position: absolute; top:0; right: 0; z-index:99999;">
-        <div style="padding: 10px; background: rgba(0,0,0,0.2); color: white; font: sans-serif">
-          <h4 style="margin:0;">Cache Debugger</h4>
-          <ul style="padding:5px; margin:0; list-style: none;">
-            ${ join(res, r => /*html*/`
-              <li ${r.style}>${r.label}</li>
-            `)}
-          </ul>
-        </div>
-      </div>
-    `)
+    ].map(({name, label}) => {
+      const header = resCacheHeaders.find(h => h.name === name);
+      return {
+        style: getStyle(!!header),
+        label,
+        value: header?.value
+      }
+    });
+    waitForBody(() => renderCacheHeaderOverlay(res));
   }
 });
 
@@ -29,4 +22,29 @@ const getStyle = (flag) => {
 
 const join = (arr, fn) => {
   return arr.map(fn).join('');
+}
+
+const waitForBody = (cb) => {
+  const sw = Date.now();
+  const i = setInterval(() => document.body ? done() : null, 25);
+  const done = () => {
+    console.log('waited for body', Date.now() - sw);
+    clearInterval(i);
+    cb();
+  }
+}
+
+const renderCacheHeaderOverlay = (res) => {
+  document.body.insertAdjacentHTML('beforeend', /*html*/`
+    <div style="position: absolute; top:0; right: 0; z-index:99999;">
+      <div style="padding: 10px; background: rgba(0,0,0,0.2); color: white; font: sans-serif">
+        <h4 style="margin:0;">Cache Debugger</h4>
+        <ul style="padding:5px; margin:0; list-style: none;">
+          ${ join(res, r => /*html*/`
+            <li ${r.style} title="${r.value}" >${r.label}</li>
+          `)}
+        </ul>
+      </div>
+    </div>
+  `)
 }
